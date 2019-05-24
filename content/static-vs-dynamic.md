89d077965b29da5038fc536e7139158fce8ae517
adriana <aspatterson@gmail.com>
2019-05-24 15:59:28 -0700

    all inital  changes

---
+++ content/static-vs-dynamic.md
@@ -0,0 +1,15 @@
+Title: Static vs. Dynamic
+Date: 2019-05-13 
+Category: General
+Tags: Python, Pelican
+Slug: static-vs-dynamic
+Authors: Adriana
+Summary: How this site exists
+
+In my recent explorations of types of sites to create: I've pretty much done them all. I've had a wordpress site, I have a squarespace site, I'm working on a complete front-to-back site, and I have this one.
+
+It just depends on the purpose of you site. This one serves purely a journalistic purpose - presenting information that is linked, but really requires no processing information other than on the surface html.
+
+I also have a squarespace site, which I love. I've always felt lacking in design capability. Some people have it, some people I don't. I'm one of the latter. Squarespace is an all-in-one package for design and content right out of the box. Maintaining it is a bit more expensive at about $8 a month, but for me it's worth it. The templates are so beautiful that I have trouble choosing one, and for my business it serves it's purpose. I needed something new and shiney to add my content too, which is the focus of my consultantcy. In that case I have no desire to change it - my main focus on that area is to focus on my science, on my writing and tutorials.
+
+On the other hand, such a site here would be lovely, but my goal is to document and present my learning, so I chose to go with a very simple static site generator - Pelican. I just really love Python so it seemed like the most rational choice. It requires a little knowledge of how Python works but for the most part it's so well documented that almost every problem is google-able.   
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,15 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Adriana'
+SITEURL = 'http://adrianapip.github.io'
 SITENAME = "Adriana's Site"
 SITEURL = ''
+SITETITLE = 'Adriana Patterson Ip'
+SITESUBTITLE = "I'm a scientist 👩🏻‍🔬 that can write a little code 👩🏻‍💻 and bake amazing pies 🥧."
+COPYRIGHT_NAME = 'Adriana Patterson Ip,'
+COPYRIGHT_YEAR = '2019'
+SITELOGO = SITEURL + '/img/profile.png'
+FAVICON = SITEURL + '/images/favicon.ico'
 
 PATH = 'content'
 
@@ -19,17 +26,30 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+MAIN_MENU = True
+DISPLAY_PAGES_ON_MENU = True
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Dranalytics', 'https://www.dranalytics.co'),
+         ('Tasty Pastry', 'https://www.tastypastry.kitchen'),
+         
+         ('Blog', '/'),)
+        
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/Adrianapip'),
+          ('twitter', 'https://twitter.com/aspatterson'),
+          ('linkedin', 'https://www.linkedin.com/in/adrianaip/'))
 
 DEFAULT_PAGINATION = 10
+
+THEME = '/Users/adrianaip/pelican-themes/Flex'
+CUSTOM_CSS = 'static/custom.css'
+STATIC_PATHS = ['img', 'static']
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
